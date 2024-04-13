f4ea2bb2a8e5f5d77a723b18568ff7715f52a137
Rafael Noll <rafanoll2004@gmail.com>
2024-04-13 14:00:18 -0300

    Initializing project

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Navbar(){
+  return (
+    <Container>
+      Nav
+    </Container>
+  )
+}
---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,7 @@
+import Navbar from "../../components/Navbar";
+
+export default function Homepage(){
+  return (
+    <Navbar />
+  )
+}
