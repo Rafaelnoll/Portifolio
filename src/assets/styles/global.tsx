7bddde1887004003d9fab30f6bc9783b9565b956
Rafael Noll <rafanoll2004@gmail.com>
2024-04-13 17:14:28 -0300

    Style: Adding background image into html body

--- src/assets/styles/global.tsx
+++ src/assets/styles/global.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import BgImage from '../images/background-dust.webp';
 
 export const GlobalStyles = createGlobalStyle`
   * {
@@ -12,5 +13,9 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.black};
     font-family: "Inter", sans-serif;
+    background-image: ${`url(${BgImage})`};
+    background-repeat: no-repeat;
+    background-size: 100%;
+    backdrop-filter: blur(5px);
   }
 `
